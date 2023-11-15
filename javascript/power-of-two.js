fa234dae0db9f375afb50e32eb419f2d9c72d5d9
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-11-14 21:25:34 -0500

    linting

--- javascript/power-of-two.js
+++ javascript/power-of-two.js
@@ -2,7 +2,7 @@ const isPowerOfTwo = function(n) {
   
   // Check for edge case - 2 to the power of 0
   if (n === 1) {
-      return true;
+    return true;
   }
 
   // Create multiple buffer
@@ -10,17 +10,17 @@ const isPowerOfTwo = function(n) {
 
   // Loop to divide multiple 2 until it yields 1 and return true or returns false
   while (multiple > 1) {
-      multiple /= 2;
-      if (multiple === 1) {
-          return true;
-      }
+    multiple /= 2;
+    if (multiple === 1) {
+      return true;
+    }
   }
 
   return false;
 };
 
-// Sample usage:
+/* Sample usage:
 console.log(isPowerOfTwo(156));
 console.log(isPowerOfTwo(1));
 console.log(isPowerOfTwo(128));
-console.log(isPowerOfTwo(65536));
+console.log(isPowerOfTwo(65536)); */
