e95e898e01085b9d3d6edd088a38d6ea0db31ef2
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-11-22 12:28:40 -0500

    lint attendance-record

--- javascript/attendance-record.js
+++ javascript/attendance-record.js
@@ -14,37 +14,37 @@ Return true if the student is eligible for an attendance award, or false otherwi
 // Main function
 const checkRecord = function(s) {
 
-    // Convert string to array
-    const sArray = s.split('');
+  // Convert string to array
+  const sArray = s.split('');
 
-    // Var  to track highest number of consecutive Ls
-    let maxLs = 0;
+  // Var  to track highest number of consecutive Ls
+  let maxLs = 0;
 
-    // Dec var to ccount number of absences 'A';
-    let aCount = 0;
+  // Dec var to ccount number of absences 'A';
+  let aCount = 0;
 
-    // Var to track current count of Ls
-    let lCount = 0;
+  // Var to track current count of Ls
+  let lCount = 0;
 
-    // Loop through array and increase aCount if element is 'A', increase lCount if element is 'L', assign lCount to maxLs if it is greater than maxLs, else reset lCount
-    for (let i = 0; i < sArray.length; i++) {
-        if(sArray[i] === 'A') {
-            aCount++;
-        }
-        if (sArray[i] === 'L') {
-            lCount++
-            maxLs = Math.max(maxLs, lCount);
-        } else {
-          lCount = 0;
-        }
+  // Loop through array and increase aCount if element is 'A', increase lCount if element is 'L', assign lCount to maxLs if it is greater than maxLs, else reset lCount
+  for (let i = 0; i < sArray.length; i++) {
+    if (sArray[i] === 'A') {
+      aCount++;
     }
-
-    // Return true if student has been absent less than twice and not late 3 consective times. Otherwise function returns false
-    if (aCount < 2 && maxLs < 3) {
-        return true;
+    if (sArray[i] === 'L') {
+      lCount++;
+      maxLs = Math.max(maxLs, lCount);
+    } else {
+      lCount = 0;
     }
+  }
 
-    return false;
+  // Return true if student has been absent less than twice and not late 3 consective times. Otherwise function returns false
+  if (aCount < 2 && maxLs < 3) {
+    return true;
+  }
+
+  return false;
 };
 
 // Example usage
