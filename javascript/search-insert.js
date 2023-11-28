6618819a95d5fec9f5269e88932b0fb1129ce955
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-11-28 12:52:47 -0500

    search-insert add  comments

--- javascript/search-insert.js
+++ javascript/search-insert.js
@@ -4,23 +4,29 @@ Given a sorted array of distinct integers and a target value, return the index i
 
 */
 
+// Main Function
 const searchInsert = function(nums, target) {
 
+  // Initialize counter variable
   let n = 0;
 
+  // Loop through nums and return n if element is equal to or greater than target
   while (n < nums.length) {
     if (nums[n] >= target) {
         return n;
     }
     n++
   }
+
+  // Return n to indicate insertion at the end of array if target is greater than all elements
   return n;
 }
 
-// Ecxample usage:
+/* Ecxample usage:
 const nums1 = [1, 3, 5, 6];
 const target1 = 2;
 const nums2 = [1, 3, 5, 6];
 const target2 = 7;
 console.log(searchInsert(nums1, target1));
 console.log(searchInsert(nums2, target2));
+*/
