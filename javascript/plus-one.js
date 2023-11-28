0be991dcc0e37e34c171949ed58deb0a0578a9c9
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-11-28 13:19:27 -0500

    plus-one

---
+++ javascript/plus-one.js
@@ -0,0 +1,31 @@
+/* Problem Description:
+
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits.
+
+*/
+
+// Main Function:
+
+const plusOne = function(digits) {
+
+  const n = digits.length;
+
+  for (let i = n - 1; i >= 0; i--) {
+    digits[i]++
+
+    if (digits[i] === 10) {
+      digits[i] = 0;
+    } else {
+      break;
+    }
+  }
+
+  if (digits[0] === 0) {
+    digits.unshift(1)
+  }
+
+  return digits;
+
+}
