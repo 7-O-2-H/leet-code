0949126d8fd3cda08c02330ec9c0f3dc4e27160b
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-11-28 12:50:34 -0500

    search-insert example usage

--- javascript/search-insert.js
+++ javascript/search-insert.js
@@ -16,3 +16,11 @@ const searchInsert = function(nums, target) {
   }
   return n;
 }
+
+// Ecxample usage:
+const nums1 = [1, 3, 5, 6];
+const target1 = 2;
+const nums2 = [1, 3, 5, 6];
+const target2 = 7;
+console.log(searchInsert(nums1, target1));
+console.log(searchInsert(nums2, target2));
