633358592681c4ef8a4663e4a50a3dd118107173
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-12-07 18:10:50 -0500

    contains-duplicates - add comments

--- javascript/contains-duplicates.js
+++ javascript/contains-duplicates.js
@@ -2,9 +2,13 @@
 Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 */
 
+// Main function
 const containsDuplicate = function(nums) {
+
+  // Create new Set
   const numSet = new Set();
 
+  // Loop through elements of nums and check set for element. Return true if element is in the set else add the number to the set.
   for (const num of nums) {
       if (numSet.has(num)) {
           return true;
@@ -12,13 +16,15 @@ const containsDuplicate = function(nums) {
       numSet.add(num);
   }
 
+  // Return false if no duplicate found.
   return false;
 };
 
 
 // Test Cases
-
+/*
 nums1 = [1, 2, 3, 4];
 nums2 = [1, 2, 3, 1];
 console.log(containsDuplicate(nums1));
 console.log(containsDuplicate(nums2));
+*/
