3861b92dfef6f408b6787798d76f0f97ebb2f12c
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-12-07 18:07:31 -0500

    contains-duplicates, add test cases

--- javascript/contains-duplicates.js
+++ javascript/contains-duplicates.js
@@ -1,3 +1,7 @@
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+*/
+
 const containsDuplicate = function(nums) {
   for (let i = 0; i < nums.length; i++) {
       let checkElement = nums[i];
@@ -10,3 +14,10 @@ const containsDuplicate = function(nums) {
 
   return false;
 };
+
+// Test Cases
+
+nums1 = [1, 2, 3, 4];
+nums2 = [1, 2, 3, 1];
+console.log(containsDuplicate(nums1));
+console.log(containsDuplicate(nums2));
