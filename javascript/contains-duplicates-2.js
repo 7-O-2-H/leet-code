a5e87bf3c814fcaba615929f88de48431111b0b3
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-12-07 18:21:46 -0500

    contains-duplicates-2 - add test cases

--- javascript/contains-duplicates-2.js
+++ javascript/contains-duplicates-2.js
@@ -11,3 +11,14 @@ const containsNearbyDuplicate = function(nums, k) {
 
   return false;
 };
+
+nums1 = [1,2,3,1];
+k1 = 3;
+nums2 = [1,0,1,1];
+k2 = 1;
+nums3 = [1,2,3,1,2,3];
+k3 = 2;
+
+console.log(containsNearbyDuplicate(nums1, k1));
+console.log(containsNearbyDuplicate(nums2, k2));
+console.log(containsNearbyDuplicate(nums3, k3));
