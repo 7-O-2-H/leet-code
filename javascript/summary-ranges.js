0c99ab98f75cc04cda3e8da6aac0054d9f8e1abf
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-12-08 15:01:44 -0500

    summary-ranges - add comments

--- javascript/summary-ranges.js
+++ javascript/summary-ranges.js
@@ -1,3 +1,18 @@
+/*
+Problem Description:
+You are given a sorted unique integer array nums.
+
+A range [a,b] is the set of all integers from a to b (inclusive).
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+"a->b" if a != b
+"a" if a == b
+*/
+
+// Main function
 const summaryRanges = function(nums) {
   if (nums.length === 0) {
       return [];
@@ -25,6 +40,7 @@ const summaryRanges = function(nums) {
   return result;
 };
 
+// This function formats the ranges into the format 'start -> end'
 function formatRange(start, end) {
   return start === end ? `${start}` : `${start}->${end}`;
 }
