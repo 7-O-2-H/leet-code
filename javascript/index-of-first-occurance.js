715e078c62ebd317b0ad9b57baae77e75a3b91f0
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-12-11 14:12:21 -0500

    index-of-first-occurance

--- javascript/index-of-first-occurance.js
+++ javascript/index-of-first-occurance.js
@@ -1,8 +1,17 @@
+// Function definition
+/*
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+*/
+
+// Main function
 const strStr = function(haystack, needle) {
+
+  // Return 0 if needle is empty string
   if (needle === '') {
       return 0; // Empty needle is always present at the beginning
   }
 
+  // Loop through haystack and compare indexes of needle and haystack
   for (let i = 0; i <= haystack.length - needle.length; i++) {
       let match = true;
 
@@ -13,6 +22,7 @@ const strStr = function(haystack, needle) {
           }
       }
 
+      // Return i (index of first element of needle) if match is true
       if (match) {
           return i;
       }
@@ -22,7 +32,7 @@ const strStr = function(haystack, needle) {
 };
 
 // Test cases
-
+/*
 const haystack1 = "sadbutsad";
 const needle1 = "sad";
 const haystack2 = "leetcode";
@@ -30,3 +40,4 @@ const needle2 = "leeto";
 
 console.log(strStr(haystack1, needle1));
 console.log(strStr(haystack2, needle2));
+*/
