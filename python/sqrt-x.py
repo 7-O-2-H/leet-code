a820c1cf9dc085871c65d53ed57f8ccc35300ff5
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-12-11 14:33:32 -0500

    sqrt-x add comments, lint, function description

--- python/sqrt-x.py
+++ python/sqrt-x.py
@@ -1,14 +1,19 @@
-def mySqrt(x) -> int:
-        if x == 0 or x == 1:
-            return x
-            
-        i = 1
+'''
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+'''
 
-        while x >= i * i:
-            i = 1 + i
-            continue
+# Main function
+def mySqrt(x) -> int:
+    if x == 0 or x == 1:
+        return x
+            
+    i = 1
+
+    while x >= i * i:
+        i = 1 + i
+        continue
         
-        return i - 1
+    return i - 1
 
 # Test cases
 '''
