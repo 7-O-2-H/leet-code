67a8bb75ff6676e76494b97292461820a6e7ac0e
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-12-11 14:09:27 -0500

    index-of-first-occurance - add test cases

--- javascript/index-of-first-occurance.js
+++ javascript/index-of-first-occurance.js
@@ -20,3 +20,13 @@ const strStr = function(haystack, needle) {
 
   return -1; // Needle not found in haystack
 };
+
+// Test cases
+
+const haystack1 = "sadbutsad";
+const needle1 = "sad";
+const haystack2 = "leetcode";
+const needle2 = "leeto";
+
+console.log(strStr(haystack1, needle1));
+console.log(strStr(haystack2, needle2));
