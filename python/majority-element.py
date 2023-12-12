8546f72f1aa56e91bb687dee6df8b50a70fd00b3
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-12-12 17:23:33 -0500

    majority-element.py - add comments

--- python/majority-element.py
+++ python/majority-element.py
@@ -1,7 +1,15 @@
+'''
+Problem description:
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+'''
+
+# Main function
 def majorityElement(self, nums: List[int]) -> int:
     candidate = nums[0]
     count = 1
     
+    # Loop through nums and use count to keep track of candidates
     for num in nums[1:]:
         if count == 0:
             candidate = num
@@ -11,4 +19,5 @@ def majorityElement(self, nums: List[int]) -> int:
         else:
             count -= 1
     
+    # Return most frequent candidate
     return candidate
