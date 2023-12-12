d33e8b015cd79fe95600e3b643925a8fcb6d91aa
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-12-12 17:21:40 -0500

    majority-element.py - fix indentation

--- python/majority-element.py
+++ python/majority-element.py
@@ -1,14 +1,14 @@
 def majorityElement(self, nums: List[int]) -> int:
-        candidate = nums[0]
-        count = 1
+    candidate = nums[0]
+    count = 1
     
-        for num in nums[1:]:
-            if count == 0:
-                candidate = num
-                count = 1
-            elif num == candidate:
-                count += 1
-            else:
-                count -= 1
+    for num in nums[1:]:
+        if count == 0:
+            candidate = num
+            count = 1
+        elif num == candidate:
+            count += 1
+        else:
+            count -= 1
     
-        return candidate
+    return candidate
