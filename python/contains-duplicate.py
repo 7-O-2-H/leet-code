a2ee2e05b16c1923ae0125b5a0982dd92cd96e87
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-12-13 18:33:13 -0500

    contains-duplicates.py - add problem description

--- python/contains-duplicate.py
+++ python/contains-duplicate.py
@@ -1,3 +1,9 @@
+'''
+Function description:
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+'''
+
 def containsDuplicate(self, nums: List[int]) -> bool:
         for i in range(len(nums)):
             check_element = nums[i]
