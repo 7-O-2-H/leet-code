e831d61628206549f78cadc3d0229e2846c8d99c
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-12-14 13:46:53 -0500

    two-sum.py - add comments

--- python/two-sum.py
+++ python/two-sum.py
@@ -1,6 +1,14 @@
+'''
+Function descrtiption:
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+'''
+
+# Main function:
 def twoSum(nums, target):
         num_indices = {}
-
+        # Loop through elements of nums
         for i, num in enumerate(nums):
             complement = target - num
 
@@ -13,8 +21,9 @@ def twoSum(nums, target):
         return None
 
 # Example usage:
-
+'''
 nums = [2, 7, 11, 15]
 target = 9
 result = twoSum(nums, target)
 print(f"Indices of the two numbers: {result}")
+'''
