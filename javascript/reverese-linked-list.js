fc9a3a82eeb76effd8df816ddedabfdeda74505f
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-01-16 16:16:42 -0500

    reverse-linked-list in JS

---
+++ javascript/reverese-linked-list.js
@@ -0,0 +1,67 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  // Add a new node to the end of the linked list
+  append(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+
+    current.next = newNode;
+  }
+
+  // Reverse the linked list
+  reverse() {
+    let prev = null;
+    let current = this.head;
+    let next = null;
+
+    while (current) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+
+    this.head = prev;
+  }
+
+  // Display the linked list elements
+  display() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
+
+// Example usage
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+
+console.log("Original linked list:");
+linkedList.display();
+
+linkedList.reverse();
+
+console.log("\nReversed linked list:");
+linkedList.display();
