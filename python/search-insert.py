366b9b93e09127d7ce0ce2ffcdc3e1fe77e304b5
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-02-15 21:47:25 -0500

    search-insert.py example usage

--- python/search-insert.py
+++ python/search-insert.py
@@ -5,18 +5,22 @@ You must write an algorithm with O(log n) runtime complexity.
 '''
 
 # Solution:
-class Solution:
-  def searchInsert(nums, target):
-    low = 0
-    high = len(nums) - 1
+def search_insert(nums, target):
+  low = 0
+  high = len(nums) - 1
 
-    while low <= high:
-      mid = (low + high) // 2
-      if nums[mid] == target:
-        return mid
-      elif nums[mid] < target:
-        low = mid + 1
-      else:
-        high = mid - 1
-
-    return low
+  while low <= high:
+    mid = (low + high) // 2
+    if nums[mid] == target:
+      return mid
+    elif nums[mid] < target:
+      low = mid + 1
+    else:
+      high = mid - 1
+      
+  return low
+  
+# Driver code:
+nums = [1, 3, 5, 6]
+target = 5
+print(search_insert(nums, target))  # Output: 2
