ab9d36bcb4da8bd6bcb3847ecf120a304427d701
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-02-19 18:08:44 -0500

    is-power-of-two.py add comments

--- python/is-power-of-two.py
+++ python/is-power-of-two.py
@@ -3,13 +3,16 @@ Given an integer n, return true if it is a power of two. Otherwise, return false
 An integer n is a power of two, if there exists an integer x such that n == 2x.
 '''
 
+# Solution
 def is_power_of_two(n):
 
+  # check if n is 1
   if n == 1:
     return True
   
   divisor = n
 
+  # divide n by 2 until it reaches 0 (return true) or if it is less than 0 (return false)
   while divisor > 1:
     divisor = divisor / 2
 
@@ -17,3 +20,4 @@ def is_power_of_two(n):
       return True
     
   return False
+
