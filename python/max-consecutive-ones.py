21237a97fe45af7828514980f03eb8032b0b10ea
Josh Kelly <jkelly85@alumni.uwo.ca>
2024-02-19 18:38:49 -0500

    max-consecutive-ones.py add comments

--- python/max-consecutive-ones.py
+++ python/max-consecutive-ones.py
@@ -5,9 +5,11 @@ Given a binary array nums, return the maximum number of consecutive 1's in the a
 # Solution
 def find_max_consecutive_ones(nums):
 
+  # set a counter and something to track most consectuive ones (highest_ones)
   highest_ones = 0
   count = 0
 
+  # loop through nums and add 1 to count if i is 1, compare highest_ones to count and set highest ones to max
   for i in nums:
 
     if i == 1:
